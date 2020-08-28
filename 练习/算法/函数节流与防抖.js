9312744c3a4ede6037620036d662e93e9daeab71
louisliu233 <1010465852@qq.com>
2020-08-28 17:00:05 +0800

    节流与防抖

---
+++ 练习/算法/函数节流与防抖.js
@@ -0,0 +1,35 @@
+//函数节流
+function throttle(fn, delay){
+    var lastTime = 0
+    return function(){
+        var nowTime = Date.now()
+        if(nowTime - lastTime > delay){
+            fn.call(this)
+            lastTime = nowTime
+        }
+    }
+    
+}
+
+function throttle(fn,delay){
+    let isRun = true
+    return function(){
+        if(!isRun)return
+        isRun = false
+        setTimeout(() => {
+            fn.call(this)
+            isRun = true
+        },delay)
+    }
+}
+
+//函数防抖
+function debounce(fn, delay){
+    let timer = null
+    return function(){
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn.call(this)
+        },delay)
+    }
+}
